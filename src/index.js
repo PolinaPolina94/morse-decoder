2945c3559b9a57f871544c94a6626a0c393d6fd5
Полина <polina_kazhina@mail.ru>
2023-02-23 18:01:45 +0300

    feat:done

--- src/index.js
+++ src/index.js
@@ -1,72 +1,69 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  ".-": "a",
+  "-...": "b",
+  "-.-.": "c",
+  "-..": "d",
+  ".": "e",
+  "..-.": "f",
+  "--.": "g",
+  "....": "h",
+  "..": "i",
+  ".---": "j",
+  "-.-": "k",
+  ".-..": "l",
+  "--": "m",
+  "-.": "n",
+  "---": "o",
+  ".--.": "p",
+  "--.-": "q",
+  ".-.": "r",
+  "...": "s",
+  "-": "t",
+  "..-": "u",
+  "...-": "v",
+  ".--": "w",
+  "-..-": "x",
+  "-.--": "y",
+  "--..": "z",
+  ".----": "1",
+  "..---": "2",
+  "...--": "3",
+  "....-": "4",
+  ".....": "5",
+  "-....": "6",
+  "--...": "7",
+  "---..": "8",
+  "----.": "9",
+  "-----": "0",
 };
 
 function decode(expr) {
-    // write your solution here
-    let result = [];
-	for (let i = 0; i < expr.length; i = i + 10) {
-		result.push(expr.slice(i, i + 10));
-	}
+  // write your solution here
+  let word = [];
+  for (let i = 0; i < expr.length; i = i + 10) {
+    word.push(expr.slice(i, i + 10));
+  }
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] == "**********") {
+      word[i] = " ";
+      continue;
+    }
+    let letter = "";
+    for (let j = 0; j < 10; j = j + 2) {
+      if (word[i][j] + word[i][j + 1] == "10") {
+        letter += ".";
+      } else if (word[i][j] + word[i][j + 1] == "11") {
+        letter += "-";
+      }
+    }
 
-	for (let i = 0; i < result.length; i++) {
-		if (result[i] == '**********') {
-			result[i] = ' ';
-			continue;
-		}
+    word[i] = MORSE_TABLE[letter];
+  }
 
-		let symbol = '';
+  return word.join("");
 
-		for (let j = 0; j < 10; j = j + 2) {
-			if (result[i][j] + result[i][j + 1] == '10') {
-				symbol += '.';
-			} else if (result[i][j] + result[i][j + 1] == '11') {
-				symbol += '-';
-			}
-		}
-
-		result[i] = MORSE_TABLE[symbol];
-	}
-
-
-	return result.join('');
 }
 
 module.exports = {
-    decode
-}
+  decode,
+};
